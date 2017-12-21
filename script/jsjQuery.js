8de3e61346f164c838edcf8a4d5cdbb94927cbdc
Olexandr Zhytenko <zhytenko@ukr.net>
2017-12-21 14:57:46 +0200

    first commit

---
+++ script/jsjQuery.js
@@ -0,0 +1,19 @@
+$(".navigation1").bind("click", function() {
+    $(".navigation1").text("Will be done").css("color", "white");
+    $(".navigation1").css("text-alighn", "center").css("padding-top", "20px");
+    $(".navigation1");
+});
+
+$(".navigation2").bind("click", function() {
+    $(".navigation2").text("Will be done").css("color", "white");
+    $(".navigation2").css("text-alighn", "center").css("padding-top", "20px");
+});
+
+$(".navigation3").bind("click", function() {
+    $(".navigation3").text("Will be done").css("color", "white");
+    $(".navigation3").css("text-alighn", "center").css("padding-top", "20px");
+});
+
+$(".submit").focus(function() {
+    $(".submit").val("Sing In hasn't work yet.");
+});
